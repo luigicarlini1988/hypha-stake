045b43c8ef50984ba782bda77a01ad47f0203a09
Luigi Carlini <luigicarlini88@gmail.com>
2024-04-20 19:06:49 +0530

    first push

---
+++ js/interactions.js
@@ -0,0 +1,19 @@
+//stake-pop-up
+function toggleModalStake() {
+    document.getElementById("stake-modal-pop").classList.toggle("show");
+    document.body.classList.toggle("no-scroll");
+}
+
+//menu
+function toggleNavMenu() {
+    document.getElementById("resp-menu-icon").classList.toggle("close");
+    document.getElementById("main-menu").classList.toggle("show");
+    document.body.classList.toggle("no-scroll");
+}
+
+//filters
+function toggleFilters() {
+    document.getElementById("filter-modal-pop").classList.toggle("show");
+    document.body.classList.toggle("no-scroll");
+}
+
